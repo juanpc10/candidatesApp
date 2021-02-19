190ed3a133ba4b3328a05723c117e8c992c73926
juanpc10 <juanp23carrillo@gmail.com>
2021-02-18 22:44:58 -0700

    final content

---
+++ README.md
@@ -0,0 +1,39 @@
+
+
+This is a sample application that checks a googgle spreadsheet and checks for new entries. New entries representing a candidates information will be contacted by email or phone. Candidates are contacted through Talkpush's api. 
+
+### Google sheet setup
+
+Download Google's credentials and copy them to this project with the filename `client_secret.json`.
+
+
+## Running the application
+
+Run `bundle install` in the general folder.
+
+Install dependencies
+`bundle install google_drive`
+
+Change APIkey and APIsecret as needed.
+
+The app checks for new entries in two ways.
+
+##### First way:
+
+* Run `bundle exec ruby button_check.rb` in the root folder.
+* Check http://localhost:4567/ 
+* Read the information displayed.
+* Click submit after entering a candidate's information in the google spreadsheet provided.
+* Check the results by receiving a call or email from the information provided.
+
+##### Second way:
+
+* Run `bundle exec ruby interval_check.rb` in the root folder
+* Add entries to https://docs.google.com/spreadsheets/d/1xkofJa5iI3AQE4yWEoHqMTQ1QQ-VDsfUDDwV96QQDVM/edit?usp=sharing .
+* The spreadsheet is checked every 5 seconds.
+* Check the results by receiving a call or email from the information provided.
+
+
+### How it works
+
+Two spreadsheets are linked. Anytime a new entry is made in the original spreadsheet https://docs.google.com/spreadsheets/d/1xkofJa5iI3AQE4yWEoHqMTQ1QQ-VDsfUDDwV96QQDVM/edit?usp=sharing , they will be added to a second spreadsheet so then entries can be compared, and then an API call is made to Talkpush's API with the candidate's information.
---
+++ button_check.rb
@@ -0,0 +1,76 @@
+
+require("bundler")
+Bundler.require
+
+
+
+get "/" do
+  erb :index
+end
+
+session = GoogleDrive::Session.from_service_account_key("client_secret.json")
+
+
+post "/" do
+  begin
+    spreadsheet = session.spreadsheet_by_title("Talkpush exercice")
+    worksheet = spreadsheet.worksheets.first
+    client_rows = worksheet.rows.length
+    puts client_rows
+
+    spreadsheet2 = session.spreadsheet_by_title("Storage Talkpush exercice")
+    worksheet2 = spreadsheet2.worksheets.first
+    backend_rows = worksheet2.rows.length
+    puts backend_rows
+
+    array1 = []
+    new_entries = worksheet.rows.last(1).each { |row| array1.push(row.last(5)) }
+    if client_rows > backend_rows
+      puts "new entry"
+      new_first_name = array1[0][1]
+      new_last_name = array1[0][2]
+      new_email = array1[0][3]
+      new_phone = array1[0][4].to_s
+      puts new_first_name
+      puts new_last_name
+      puts new_email
+      puts new_phone
+      worksheet2.insert_rows(worksheet2.num_rows + 1, 
+          array1
+      )
+      worksheet2.save
+
+      uri = URI.parse("https://my.talkpush.com/api/talkpush_services/campaigns/4339/campaign_invitations")
+      request = Net::HTTP::Post.new(uri)
+      request.content_type = "application/json"
+      request["Cache-Control"] = "no-cache"
+      request.body = JSON.dump({
+        "api_key" => "abcde12345",
+        "api_secret" => "abcde12345",
+        "campaign_invitation" => {
+          "first_name" => new_first_name,
+          "last_name" => new_last_name,
+          "email" => new_email,
+          "user_phone_number" => new_phone
+        }
+      })
+      req_options = {
+        use_ssl: uri.scheme == "https",
+      }
+      response = Net::HTTP.start(uri.hostname, uri.port, req_options) do |http|
+        http.request(request)
+      end
+      puts response.code
+      puts response.body
+
+    end
+    erb :thanks
+
+
+  rescue
+    erb :index, locals: {
+      error_message: "Please try again, details could not be saved"
+    }
+  end
+end
+
